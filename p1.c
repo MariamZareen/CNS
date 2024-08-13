673012c1d7b9f018730899b8df731066ac80c432
Mariam Zareen <119798530+MariamZareen@users.noreply.github.com>
2024-08-13 21:06:41 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,22 +1,38 @@
-#include <stdio.h> 
-#include<string.h> 
-#include<stdlib.h> 
-int main() { 
-  char str[]="Hello World"; 
-  int len=strlen(str);  
-   
-  printf("After and operation\n"); 
-  for(int i=0;i<len;i++){ 
-      printf("%c=%d\n",str[i],str[i]&127); 
-  } 
-   
-   printf("After or operation\n"); 
-  for(int i=0;i<len;i++){ 
-      printf("%c=%d\n",str[i],str[i]|127); 
-  } 
-   
-   printf("After xor operation\n"); 
-  for(int i=0;i<len;i++){ 
-      printf("%c=%d\n",str[i],str[i]^127); 
-  } 
-} 
+//pg1
+#include<stdio.h>
+#include<string.h>
+
+int plen(char *p){
+    int len=0;
+    while(p[len]!='\0')
+    len++;
+    return len;
+}
+int main(){
+    char p[100];
+    printf("enter string: ");
+    fgets(p,sizeof(p),stdin);
+    
+  if(p[plen(p)-1]=='\n')
+  p[plen(p)-1]='\0';
+    
+    printf("\nafter performing and op\n");
+    for(int i=0; p[i]!='\0'; i++){
+        printf("%c = %d", p[i], p[i] & 127);
+        printf("\n");
+    }
+    
+    printf("\nafter performing or op\n");
+    for(int i=0; p[i]!='\0'; i++){
+        printf("%c = %d", p[i], p[i] | 127);
+        printf("\n");
+    }
+    
+    printf("\nafter performing xor op\n");
+    for(int i=0; p[i]!='\0'; i++){
+        printf("%c = %d", p[i], p[i] ^ 127);
+        printf("\n");
+    }
+    
+    return 0;
+}
