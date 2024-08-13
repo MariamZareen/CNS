7efb896a9962b07c080c2effd1ecaa39b7865e8b
Mariam Zareen <119798530+MariamZareen@users.noreply.github.com>
2024-08-13 11:15:08 +0530

    Update p3b.c

--- p3b.c
+++ p3b.c
@@ -1,65 +1,88 @@
-#include<stdio.h>  
-#include<string.h>  
-#include<stdlib.h>  
-char str[100]; 
-void encrypt(char *str,int rails){ 
-    printf("enter message:"); 
-    scanf(" %[^\n]", str);  
-    printf("enter no of rails:"); 
-    scanf("%d",&rails); 
-    int i,j=0,len,count=0,code[100][1000]; 
-    len=strlen(str); 
-    for(int i=0;i<rails;i++){ 
-        for(int j=0;j<len;j++){ 
-            code[i][j]=0; 
-        } 
-    } 
-    while(j<len){ 
-        if(count%2==0){ 
-            for( i=0;i<rails;i++){ 
-                if(j<len){ 
-                    code[i][j]=(int)str[j]; 
-                } 
-                j++; 
-            } 
-            } 
-            else{ 
-                for(i=rails-2;i>0;i--){ 
-                    if(j<len){ 
-                    code[i][j]=(int)str[j]; 
-                } 
-                j++; 
-                } 
-            } 
-            count++; 
-        } 
-        printf("Railfence pattern\n"); 
-        for(int i=0;i<rails;i++){ 
-            for(int j=0;j<len;j++){ 
-                if(code[i][j]!=0){ 
-                    printf("%c",code[i][j]); 
-                } 
-                else{ 
-                    printf(" "); 
-                } 
-            } 
-            printf("\n"); 
-        } 
-        printf("Encrypted text:"); 
-        for(int i=0;i<rails;i++){ 
-            for(int j=0;j<len;j++){ 
-                if(code[i][j]!=0){ 
-                    printf("%c",(char)code[i][j]); 
-                } 
-            } 
-        } 
-        printf("\n"); 
-    } 
-void decrypt(){ 
-    printf("Decrypted text:%s",str); 
-} 
-int main(){ 
-    int rails; 
-    encrypt(str,rails); 
-    decrypt(); 
-} 
+public class RailFenceCipher {
+    
+    public static String encrypt(String text, int rails) {
+        StringBuilder[] fences = new StringBuilder[rails];
+        for (int i = 0; i < rails; i++) {
+            fences[i] = new StringBuilder();
+        }
+        
+        int rail = 0;
+        boolean down = true;
+        for (char c : text.toCharArray()) {
+            fences[rail].append(c);
+            if (down) {
+                rail++;
+                if (rail == rails - 1) {
+                    down = false;
+                }
+            } else {
+                rail--;
+                if (rail == 0) {
+                    down = true;
+                }
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder fence : fences) {
+            result.append(fence);
+        }
+        return result.toString();
+    }
+    
+    public static String decrypt(String text, int rails) {
+        StringBuilder[] fences = new StringBuilder[rails];
+        for (int i = 0; i < rails; i++) {
+            fences[i] = new StringBuilder();
+        }
+        
+        int rail = 0;
+        boolean down = true;
+        int[] counts = new int[rails];
+        for (int i = 0; i < text.length(); i++) {
+            fences[rail].append(text.charAt(i));
+            if (down) {
+                rail++;
+                if (rail == rails - 1) {
+                    down = false;
+                }
+            } else {
+                rail--;
+                if (rail == 0) {
+                    down = true;
+                }
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+        rail = 0;
+        down = true;
+        for (int i = 0; i < text.length(); i++) {
+            result.append(fences[rail].charAt(counts[rail]));
+            counts[rail]++;
+            if (down) {
+                rail++;
+                if (rail == rails - 1) {
+                    down = false;
+                }
+            } else {
+                rail--;
+                if (rail == 0) {
+                    down = true;
+                }
+            }
+        }
+        
+        return result.toString();
+    }
+    
+    public static void main(String[] args) {
+        String plaintext = "Hello, World!";
+        int rails = 3;
+        String encrypted = encrypt(plaintext, rails);
+        System.out.println("Encrypted: " + encrypted);
+        
+        String decrypted = decrypt(encrypted, rails);
+        System.out.println("Decrypted: " + decrypted);
+    }
+}
